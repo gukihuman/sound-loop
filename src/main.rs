ac25b550ee798e4c61d36616d5c99bec6bfffa13
gukihuman <sokolovivanf@gmail.com>
2024-10-15 16:03:05 +0500

    fix cracks with cpal buffer size

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,75 @@
-#![windows_subsystem = "windows"] // Add this line at the beginning
+#![windows_subsystem = "windows"]
 
+use anyhow::Result;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{BufferSize, SampleRate, StreamConfig};
+use rodio::{Decoder, Source};
 use std::io::Cursor;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
-
-use rodio::{Decoder, OutputStream, Sink, Source};
 use tray_item::{IconSource, TrayItem};
 
-fn main() {
+const BUFFER_SIZE: u32 = 8192; // 8192, 16384
+
+fn main() -> Result<()> {
     let sound_data = include_bytes!("sound.mp3");
     let cursor = Cursor::new(sound_data);
 
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+
+    let config = StreamConfig {
+        channels: 1,
+        sample_rate: SampleRate(48000),
+        buffer_size: BufferSize::Fixed(BUFFER_SIZE),
+    };
+
+    println!("Using config: {:?}", config);
+
+    let source = Decoder::new(cursor)?;
+    let channels = source.channels();
+
+    let samples: Vec<f32> = source.convert_samples().collect();
+    let samples = Arc::new(Mutex::new(samples));
+    let sample_index = Arc::new(Mutex::new(0));
+
     let sound_on_off = Arc::new(Mutex::new(true));
-    let volume_level = Arc::new(Mutex::new(0.35)); // Default volume: 10%
+    let volume_level = Arc::new(Mutex::new(0.25f32));
 
-    // Initialize audio output and decoder
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    let source = Decoder::new(cursor).unwrap().repeat_infinite();
+    let samples_clone = Arc::clone(&samples);
+    let sample_index_clone = Arc::clone(&sample_index);
+    let sound_on_off_clone = Arc::clone(&sound_on_off);
+    let volume_level_clone = Arc::clone(&volume_level);
 
-    // Play the sound initially
-    sink.set_volume(*volume_level.lock().unwrap());
-    sink.append(source);
-    sink.play();
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            let mut index = sample_index_clone.lock().unwrap();
+            let samples = samples_clone.lock().unwrap();
+            let is_on = *sound_on_off_clone.lock().unwrap();
+            let volume = *volume_level_clone.lock().unwrap();
+
+            for frame in data.chunks_mut(channels as usize) {
+                for (i, sample) in frame.iter_mut().enumerate() {
+                    *sample = if is_on {
+                        samples[*index * channels as usize + i] * volume
+                    } else {
+                        0.0
+                    };
+                }
+                *index += 1;
+                if *index >= samples.len() / channels as usize {
+                    *index = 0;
+                }
+            }
+        },
+        |err| eprintln!("an error occurred on stream: {}", err),
+        None,
+    )?;
+
+    stream.play()?;
 
     // Create a tray icon
     let mut tray = TrayItem::new("sound-loop", IconSource::Resource("icon_on")).unwrap();
@@ -34,34 +81,27 @@ fn main() {
     let tx_clone = tx.clone();
     tray.add_menu_item("10%", move || {
         tx_clone.send(0.10).unwrap();
-    })
-    .unwrap();
+    })?;
 
     let tx_clone = tx.clone();
-    tray.add_menu_item("35%  <", move || {
-        tx_clone.send(0.35).unwrap();
-    })
-    .unwrap();
+    tray.add_menu_item("25%  <", move || {
+        tx_clone.send(0.25).unwrap();
+    })?;
 
     let tx_clone = tx.clone();
     tray.add_menu_item("100%", move || {
         tx_clone.send(1.00).unwrap();
-    })
-    .unwrap();
-
-    tray.inner_mut().add_separator().unwrap();
+    })?;
 
     // --- On/Off Menu Item ---
     let tx_clone = tx.clone();
     tray.add_menu_item("On/Off", move || {
         tx_clone.send(-1.0).unwrap();
-    })
-    .unwrap();
+    })?;
 
     tray.add_menu_item("Exit", move || {
         std::process::exit(0);
-    })
-    .unwrap();
+    })?;
 
     // Main loop to handle tray menu events
     loop {
@@ -72,28 +112,22 @@ fn main() {
                 *is_on = !*is_on;
 
                 if *is_on {
-                    sink.play();
-                    tray.set_icon(IconSource::Resource("icon_on")).unwrap();
+                    tray.set_icon(IconSource::Resource("icon_on"))?;
                 } else {
-                    sink.pause();
-                    tray.set_icon(IconSource::Resource("icon_off")).unwrap();
+                    tray.set_icon(IconSource::Resource("icon_off"))?;
                 }
             } else {
                 // Volume change
                 let mut vol = volume_level.lock().unwrap();
                 *vol = new_volume;
-                sink.set_volume(*vol);
 
                 // Update volume menu labels
                 tray.inner_mut()
-                    .set_menu_item_label(if new_volume == 0.10 { "10%  <" } else { "10%" }, 0)
-                    .unwrap();
+                    .set_menu_item_label(if new_volume == 0.10 { "10%  <" } else { "10%" }, 0)?;
                 tray.inner_mut()
-                    .set_menu_item_label(if new_volume == 0.35 { "35%  <" } else { "35%" }, 1)
-                    .unwrap();
+                    .set_menu_item_label(if new_volume == 0.25 { "25%  <" } else { "25%" }, 1)?;
                 tray.inner_mut()
-                    .set_menu_item_label(if new_volume == 1.00 { "100% <" } else { "100%" }, 2)
-                    .unwrap();
+                    .set_menu_item_label(if new_volume == 1.00 { "100% <" } else { "100%" }, 2)?;
             }
         } else {
             thread::sleep(std::time::Duration::from_millis(100));
